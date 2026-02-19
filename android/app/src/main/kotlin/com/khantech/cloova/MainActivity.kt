8a4ce5319305acbd126f5cdc17f0ccd287e50aa9
HanZhang <qingeneral@gmail.com>
2026-02-19 10:13:06 +0800

    refactor: Update application bundle identifier and package name across platforms.

---
+++ android/app/src/main/kotlin/com/khantech/cloova/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.khantech.cloova
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/whattowear/what_to_wear_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.whattowear.what_to_wear_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
